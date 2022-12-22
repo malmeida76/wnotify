aeee3b6f6c2f7c4c012c5f00b8e46e3f8e51c911
Marcus Almeida <92253558+malmeida76@users.noreply.github.com>
2022-12-22 15:49:16 -0300

    Create zapbackup.sh

---
+++ zapbackup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SCRIPT DE INTEGRAÇÃO PARA OS CLIENTES DO SISTEMA DE GESTÃO DE COBRANÇA E MONITORAMENTO
+# MARCUS ALMEIDA
+# marcusalmeida@gmx.us
+# (21) 3527-0712
+
+
+#TELEFONE OU GRUPO WHATSAPP QUE VAI RECEBER O BACKUP
+#120363023073831734@g.us grupo
+#552135270712@c.us teefone
+WhatsAppTo="120363023073831734@g.us"
+
+BkpFile=$(ls -t /opt/mk-auth/bckp/ | head -1)
+
+echo $BkpFile
+
+BkpFullPathName="/opt/mk-auth/bckp/$BkpFile"
+
+echo $BkpFullPathName
+
+if [ -a $BkpFullPathName ]
+then
+    curl -v -F "To=$WhatsAppTo" -F "document=@$BkpFullPathName" "http://wa-server.us.to:8123/UPLOAD?"
+fi
+
+exit 0
+
